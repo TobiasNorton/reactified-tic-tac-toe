47a80a33c53c7048561a288d092003f38753ea8d
Tobias Norton <tnorton20@yahoo.com>
2018-11-26 11:45:15 -0500

    Using className to indicate where the user clicked

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      board: ['', '', '', '', '', '', '', '', '']
+    }
+  }
+
+  _click = event => {
+    const index = parseInt(event.target.className)
+    this.state.board[index] = 'X'
+    this.setState({
+      board: this.state.board
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Tic Tac Toe</h1>
+        <div className="board">
+          <div className="row">
+            <div className="0" onClick={this._click}>
+              {this.state.board[0]}
+            </div>
+            <div className="1" onClick={this._click}>
+              {this.state.board[1]}
+            </div>
+            <div>{this.state.board[2]}</div>
+          </div>
+          <div className="row">
+            <div>{this.state.board[3]}</div>
+            <div>{this.state.board[4]}</div>
+            <div>{this.state.board[5]}</div>
+          </div>
+          <div className="row">
+            <div>{this.state.board[6]}</div>
+            <div>{this.state.board[7]}</div>
+            <div>{this.state.board[8]}</div>
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
